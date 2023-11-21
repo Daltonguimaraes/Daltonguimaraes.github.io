eb68bafec7432165537b54540f59c7044ed95e61
Dalton Gui <02762829224>
2023-11-21 10:57:35 -0400

    Inclusão do menu assuntos

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Abre o menu dropdown ao clicar no botão
+document.querySelector(".dropdown-toggle").addEventListener("click", function() {
+    document.querySelector(".dropdown-menu").classList.toggle("show");
+});
+  
